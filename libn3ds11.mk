4f485f8e450372d4cc247242eb9ea45e7562272e
profi200 <fd3194@gmx.de>
2022-07-08 00:04:15 +0200

    Renamed thirdparty to libraries.

--- libn3ds11.mk
+++ libn3ds11.mk
@@ -1,4 +1,4 @@
 SOURCES		+=	../libn3ds/kernel/source ../libn3ds/source ../libn3ds/source/drivers/mmc \
 				../libn3ds/source/drivers ../libn3ds/source/arm11 ../libn3ds/source/arm11/allocator \
 				../libn3ds/source/arm11/drivers ../libn3ds/source/arm11/util/rbtree
-INCLUDES	+=	../libn3ds/include ../libn3ds/kernel/include ../libn3ds/thirdparty
+INCLUDES	+=	../libn3ds/include ../libn3ds/kernel/include ../libn3ds/libraries
