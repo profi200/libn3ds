e373af7018702ebab0501dec6f5d7fabb403ab5a
profi200 <fd3194@gmx.de>
2023-11-29 17:45:16 +0100

    Use info LED for silent exceptions. Also delay 2 seconds to prevent immediate power off.

--- source/arm11/debug.c
+++ source/arm11/debug.c
@@ -1,6 +1,6 @@
 /*
  *   This file is part of open_agb_firm
- *   Copyright (C) 2021 derrek, profi200
+ *   Copyright (C) 2023 derrek, profi200
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -21,36 +21,90 @@
 #include <string.h>
 #include "types.h"
 #include "mem_map.h"
+#include "arm11/drivers/i2c.h"
+#include "arm11/drivers/cfg11.h"
 #include "arm11/console.h"
 #include "arm11/fmt.h"
 #include "drivers/pxi.h"
 #include "ipc_handler.h"
 #include "arm11/drivers/interrupt.h"
 #include "arm.h"
+#include "util.h"
 #include "arm11/drivers/mcu.h"
 #include "arm11/drivers/hid.h"
 
 
+#define LED_RGB8(r, g, b) ((b)<<16 | (g)<<8 | (r))
 
-NOINLINE noreturn void panic(void)
+
+
+// Maximum value for frameMs is 500.
+// Make sure the last frame is 0%/off otherwise the LED stays on.
+static void exceptionInfoLedBlink(const u16 frameMs, const bool smooth, const u32 color, u32 patt)
 {
+	// This fixed point calculation is tested and works perfectly from 0-500 ms.
+	const u32 frameTime = (4294968u * frameMs)>>23; // 512u * frameMs / 1000.
+
+	// frameTime 0 is equal to 256 (wraps around).
+	// lastRepeat is set to 255 to repeat the last frame forever (plays whole pattern once).
+	InfoLedPattern p;
+	p.frameTime  = frameTime & 0xFFu;
+	p.rampTime   = (smooth ? (frameTime > 255 ? 255 : frameTime) : 0);
+	p.lastRepeat = 255;
+	p.unused     = 0;
+
+	// Note: First pattern is skipped by the MCU due to a firmware bug.
+	for(unsigned i = 0; i < 32; i++)
+	{
+		const u32 c = ((patt & 1u) == 1 ? color : 0u);
+		p.r[i] = c;
+		p.g[i] = c>>8;
+		p.b[i] = c>>16;
+
+		patt >>= 1;
+	}
+
+	// Since we don't know which state the CPU is in
+	// we have to use the interrupt safe I2C function.
+	I2C_writeRegArrayIntSafe(I2C_DEV_CTR_MCU, MCU_REG_INFO_LED, &p, sizeof(p));
+}
+
+static void delayFor2seconds(void)
+{
+	// If we are on New 2/3DS (LGR2) the clock may be at 804 MHz.
+	// If it's not at 804 MHz this will delay for 6 seconds.
+	const u32 multiplier = (getCfg11Regs()->socinfo & SOCINFO_LGR2 ? 3 : 1);
+	wait_cycles((268111856u * multiplier) * 2);
+}
+
+[[noreturn]] NOINLINE void panic(void)
+{
+	// Disable interrupts.
 	enterCriticalSection();
+
+	// Make the info LED blink for ~2 seconds in case this is a silent panic.
+	exceptionInfoLedBlink(65, false, LED_RGB8(128, 128, 0), 0x7FFFFFFF);
 
 	consoleInit(GFX_LCD_BOT, NULL);
 	ee_printf("\x1b[41m\x1b[0J\x1b[15C****PANIC!!!****\n");
 
 	PXI_sendPanicCmd(IPC_CMD9_PREPARE_POWER);
 
-	// Wait for A/B/X or Y
+	// Wait 2 seconds and then wait for A/B/X/Y.
+	delayFor2seconds();
 	while(!(REG_HID_PAD & (KEY_A | KEY_B | KEY_X | KEY_Y)));
 
 	MCU_sysPowerOff();
 	while(1) __wfi();
 }
 
-NOINLINE noreturn void panicMsg(const char *msg)
+[[noreturn]] NOINLINE void panicMsg(const char *msg)
 {
+	// Disable interrupts.
 	enterCriticalSection();
+
+	// Make the info LED blink for ~2 seconds in case this is a silent panic.
+	exceptionInfoLedBlink(65, false, LED_RGB8(128, 128, 0), 0x7FFFFFFF);
 
 	consoleInit(GFX_LCD_BOT, NULL);
 	ee_printf("\x1b[41m\x1b[0J\x1b[15C****PANIC!!!****\n\n");
@@ -58,7 +112,8 @@ NOINLINE noreturn void panicMsg(const char *msg)
 
 	PXI_sendPanicCmd(IPC_CMD9_PREPARE_POWER);
 
-	// Wait for A/B/X or Y
+	// Wait 2 seconds and then wait for A/B/X/Y.
+	delayFor2seconds();
 	while(!(REG_HID_PAD & (KEY_A | KEY_B | KEY_X | KEY_Y)));
 
 	MCU_sysPowerOff();
@@ -66,9 +121,12 @@ NOINLINE noreturn void panicMsg(const char *msg)
 }
 
 // Expects the registers in the exception stack to be in the following order:
-// r0-r14, pc (unmodified), CPSR, DFSR, IFSR, FAR, WFAR
-NOINLINE noreturn void guruMeditation(u8 type, const u32 *excStack)
+// r0-r14, pc (unmodified), CPSR, DFSR, IFSR, FAR, WFAR.
+[[noreturn]] NOINLINE void guruMeditation(u8 type, const u32 *excStack)
 {
+	// Make the info LED blink for ~2 seconds in case this is a silent exception.
+	exceptionInfoLedBlink(65, false, LED_RGB8(128, 0, 0), 0x7FFFFFFF);
+
 	const char *const typeStr[3] = {"Undefined instruction", "Prefetch abort", "Data abort"};
 	u32 realPc, instSize = 4;
 
@@ -111,7 +169,8 @@ NOINLINE noreturn void guruMeditation(u8 type, const u32 *excStack)
 
 	PXI_sendPanicCmd(IPC_CMD9_PREPARE_POWER);
 
-	// Wait for A/B/X or Y
+	// Wait 2 seconds and then wait for A/B/X/Y.
+	delayFor2seconds();
 	while(!(REG_HID_PAD & (KEY_A | KEY_B | KEY_X | KEY_Y)));
 
 	MCU_sysPowerOff();
@@ -127,7 +186,7 @@ __attribute__((used)) uintptr_t __stack_chk_guard = 0xC724B66D;
 // Needs to be marked as noinline and used to work with LTO.
 // The used attribute also overrides the newlib symbol.
 // Combine -fstack-protector-all with -fno-inline to get the most effective detection.
-__attribute__((noinline, used)) noreturn void __stack_chk_fail(void)
+[[noreturn]] __attribute__((noinline, used)) void __stack_chk_fail(void)
 {
 	panicMsg("Stack smash!");
 }
